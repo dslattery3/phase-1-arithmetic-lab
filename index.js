cc6f791de4e7236becdffd34f723074a50d23840
Dalton <slattery.da@gmail.com>
2022-01-10 17:08:08 -0600

    commit tree 1de4f1fc26bce3e2006368eb0d6ed0091e59c046

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16;
+const num4 = 6;
+let mod = num3 % num4;
+
+const num5 = 20;
+let max = Math.max(num1, num3, num4, num5);
